1673d32513cb03ad391f56e550157f674e686aaf
Bob D'Errico <bob@bobderrico.com>
2016-12-15 00:53:17 -0500

    Creates simple db CRUD functional test

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 
 module.exports = function(config) {
   var models = require('./models')(config);
-
   var dbServices = {};
+  dbServices.models = models;
 
-  dbServices.categoryService = require('./services/categoryService')(models);
-
-  return dbServices;
+  return [
+    'dbService',
+    'baseReminderService',
+    'timeframeService',
+    'testService',
+    'testDateService',
+    'contentItemService',
+    'schoolService',
+    'reminderService',
+    'userService',
+    'adminUserService'
+  ].reduce(function(services, service) {
+    services[service] = require('./services/' + service)(models);
+    return services;
+  }, dbServices);
 };
