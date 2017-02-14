0f7f3c6edeb4e93fd792891c48a6e40f46a840d0
Bob D'Errico <bob@bobderrico.com>
2017-02-13 22:45:00 -0500

    Ensures models are only loaded once to prevent memory leaks

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var models;
+
 module.exports = function(config) {
-  var models = require('./models')(config);
+  if (!models) {
+    models = require('./models')(config);
+  }
   var dbServices = {};
   dbServices.models = models;
 
